60fadf3858669d9787dfec2124bdebe2a47c0c1a
kyeru <hyungjuneim@gmail.com>
2025-05-11 16:00:45 +0900

    mongodb configuration update

--- src/main/java/com/oss_prototype/db_utils/MongoConfig.java
+++ src/main/java/com/oss_prototype/db_utils/MongoConfig.java
@@ -1,49 +1,27 @@
 package com.oss_prototype.db_utils;
 
+import com.mongodb.ConnectionString;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import java.util.Collections;
+import java.util.Objects;
 
 @Configuration
 public class MongoConfig {
-    @Value("${spring.data.mongodb.host}")
-    private String host;
-
-    @Value("${spring.data.mongodb.port}")
-    private int port;
-
-    @Value("${spring.data.mongodb.database}")
-    private String database;
-
-    @Value("${spring.data.mongodb.username}")
-    private String username;
-
-    @Value("${spring.data.mongodb.password}")
-    private String password;
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
 
     @Bean
     public MongoClient mongoClient() {
-        MongoCredential credential = MongoCredential.createCredential(
-            username, database, password.toCharArray());
-
-        return MongoClients.create(
-            com.mongodb.MongoClientSettings.builder()
-                .applyToClusterSettings(builder ->
-                    builder.hosts(Collections.singletonList(new ServerAddress(host, port))))
-            .credential(credential)
-            .build()
-        );
+        return MongoClients.create(uri);
     }
 
     @Bean
     public MongoTemplate mongoTemplate(MongoClient mongoClient) {
-        return new MongoTemplate(mongoClient, database);
+        return new MongoTemplate(mongoClient, Objects.requireNonNull(new ConnectionString(uri).getDatabase()));
     }
 }
